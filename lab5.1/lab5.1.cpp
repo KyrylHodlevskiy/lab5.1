92cada3cf7fc252a50c798d26269fcc4f5e8d20c
KyrylHodlevskiy <kn2004k@gmail.com>
2021-10-21 01:15:20 +0300

    first change

---
+++ lab5.1/lab5.1.cpp
@@ -0,0 +1,18 @@
+﻿// Lab 5_1
+#include <iostream>
+#include <cmath>
+using namespace std;
+double k(const double x, const double y); // прототип
+int main()
+{
+	double p, q;
+	cout << "p = "; cin >> p;
+	cout << "q = "; cin >> q;
+	double r = (pow(k(1 + p, q * q), 2) - k(q * p, 1)) / (1 + k(p * p, q));
+	cout << "r = " << r << endl;
+	return 0;
+}
+double k(const double x, const double y) // визначення
+{
+	return (sin(x) / pow(y, 2)) + ((cos(y) / pow(x, 2)));
+}
